274398efc84a1eadba92263038db5add5bb57e2d
pratiklohar <pratikwed@gmail.com>
2025-04-18 11:51:49 +0530

    Initial commit

---
+++ src/main/java/com/myapp/springbootmongodbapp/SpringbootMongodbAppApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootmongodbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/springbootmongodbapp/SpringbootMongodbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootmongodbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
