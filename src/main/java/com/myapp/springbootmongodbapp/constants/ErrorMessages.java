6f32d4a723f7608796ef2e8494dc7bbdf406a27a
pratiklohar <pratikwed@gmail.com>
2025-05-03 15:49:17 +0530

    changes

--- src/main/java/com/myapp/springbootmongodbapp/constants/ErrorMessages.java
+++ src/main/java/com/myapp/springbootmongodbapp/constants/ErrorMessages.java
@@ -2,7 +2,7 @@ package com.myapp.springbootmongodbapp.constants;
 
 public final class ErrorMessages {
     public static final String PRODUCT_NOT_FOUND = "Product not found";
-    public static final String PRODUCT_EXIST = "Product already exists";
+    public static final String PRODUCT_EXIST = "Product exists";
     public static final String CUSTOMER_NOT_FOUND = "Customer not found";
     public static final String CUSTOMER_EXIST = "Customer exists";
 }
